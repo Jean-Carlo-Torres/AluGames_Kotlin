de057bdabab3c6a5f6da514b4545fbc8288ebd94
Jean Carlo <jeansanto2002@gmail.com>
2024-03-21 10:03:48 -0300

    Listando os jogos buscados

--- src/main/kotlin/br/com/alugames/models/Gamer.kt
+++ src/main/kotlin/br/com/alugames/models/Gamer.kt
@@ -1,5 +1,6 @@
 package br.com.alugames.models
 
+import java.util.*
 import kotlin.random.Random
 
 data class Gamer(var nome: String, var email: String) {
@@ -7,13 +8,14 @@ data class Gamer(var nome: String, var email: String) {
     var usuario: String? = null
         set(value) {
             field = value
-            if (idInterno.isNullOrBlank()){
+            if (idInterno.isNullOrBlank()) {
                 criarIdInterno()
             }
         }
     var idInterno: String? = null
         private set
 
+    val jogosBuscados = mutableListOf<Jogo?>()
 
     constructor(nome: String, email: String, dataDeNascimento: String, usuario: String) : this(
         nome,
@@ -25,7 +27,7 @@ data class Gamer(var nome: String, var email: String) {
     }
 
     init {
-        if (nome.isNullOrBlank()){
+        if (nome.isNullOrBlank()) {
             throw IllegalArgumentException("Nome esta em branco")
         }
         this.email = validarEmail()
@@ -39,7 +41,7 @@ data class Gamer(var nome: String, var email: String) {
                 "idInterno=$idInterno)"
     }
 
-    fun criarIdInterno(){
+    fun criarIdInterno() {
         val numero = Random.nextInt(10000)
         val tag = String.format("%04d", numero)
 
@@ -52,6 +54,27 @@ data class Gamer(var nome: String, var email: String) {
             return email
         } else {
             throw IllegalArgumentException("Email inválido")
+        }
+    }
+
+    companion object {
+        fun criarGamer(leitura: Scanner): Gamer {
+            println("Boas vindas ao AluGames! Vamos fazer seu cadastro. Digite seu nome: ")
+            val nome = leitura.nextLine()
+            println("Digite seu email: ")
+            val email = leitura.nextLine()
+            println("Deseja completar seu cadastro com usuário e data de nascimento? S/N")
+            val opcao = leitura.nextLine()
+            if (opcao.equals("s", true)) {
+                println("Digite seu nome de usuário: ")
+                val usuario = leitura.nextLine()
+                println("Digite sua data de nascimento(DD/MM/AAAA): ")
+                val dataDeNascimento = leitura.nextLine()
+
+                return Gamer(nome, email, usuario, dataDeNascimento)
+            } else {
+                return Gamer(nome, email)
+            }
         }
     }
 }
--- src/main/kotlin/br/com/alugames/principal/Main.kt
+++ src/main/kotlin/br/com/alugames/principal/Main.kt
@@ -1,45 +1,57 @@
 package br.com.alugames.principal
 
+import br.com.alugames.models.Gamer
 import br.com.alugames.models.Jogo
 import br.com.alugames.services.ConsumoApi
 import java.util.*
 
 fun main() {
     val leitura = Scanner(System.`in`)
-    println("Digite um código de jogo para buscar:")
-    val busca = leitura.nextLine()
+    val gamer = Gamer.criarGamer(leitura)
+    println("Cadastro realizado com sucesso!")
+    println(gamer)
 
-    val buscaApi = ConsumoApi()
-    val informacaoJogo = buscaApi.buscaJogo(busca)
+    do {
+        println("Digite um código de jogo para buscar:")
+        val busca = leitura.nextLine()
 
-    var meuJogo: Jogo? = null
+        val buscaApi = ConsumoApi()
+        val informacaoJogo = buscaApi.buscaJogo(busca)
 
-    val resultado = runCatching {
-        meuJogo = Jogo(
-            informacaoJogo.info.title,
-            informacaoJogo.info.thumb
-        )
-    }
+        var meuJogo: Jogo? = null
 
-    resultado.onFailure {
-        println("Jogo inexistente. Tente outro id.")
-    }
-
-    resultado.onSuccess {
-        println("Deseja inserir uma descrição personalizada? S/N")
-        val opcao = leitura.nextLine()
-        if (opcao.equals("s", true)) {
-            println("Insira a descrição personalizado para o jogo:")
-            val descricaoPersonalizada = leitura.nextLine()
-            meuJogo?.descricao = descricaoPersonalizada
-        } else {
-            meuJogo?.descricao = meuJogo?.titulo
-
+        val resultado = runCatching {
+            meuJogo = Jogo(
+                informacaoJogo.info.title,
+                informacaoJogo.info.thumb
+            )
         }
-        println(meuJogo)
-    }
 
-    resultado.onSuccess {
-        println("Busca finalizada com sucesso.")
+        resultado.onFailure {
+            println("Jogo inexistente. Tente outro id.")
+        }
+
+        resultado.onSuccess {
+            println("Deseja inserir uma descrição personalizada? S/N")
+            val opcao = leitura.nextLine()
+            if (opcao.equals("s", true)) {
+                println("Insira a descrição personalizado para o jogo:")
+                val descricaoPersonalizada = leitura.nextLine()
+                meuJogo?.descricao = descricaoPersonalizada
+            } else {
+                meuJogo?.descricao = meuJogo?.titulo
+            }
+            gamer.jogosBuscados.add(meuJogo)
+        }
+
+        println("Deseja buscar um novo jogo? S/N")
+        val resposta = leitura.nextLine()
+    } while (resposta.equals("s", true))
+
+    println("Jogos buscados: ")
+    gamer.jogosBuscados.forEach {
+        println(it)
+        println("------------------------------------")
     }
+    println("Busca finalizada com sucesso.")
 }
